000203d1946f8463dd70f4a37d628d867de9d1d7
Taufique Iradukunda <taufiqueira@Taufiques-MacBook-Pro.local>
2024-01-09 21:29:51 -0500

    added function that take note number, add Expanded widget to every button, crossAxis to the column

--- lib/main.dart
+++ lib/main.dart
@@ -5,17 +5,105 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: TextButton(
-            onPressed: () {
-              final player = AudioPlayer();
-              player.play(AssetSource('note1.wav'));
-              //player.play('note1.wav');
-            },
-            child: Text('Press Me'),
-          ),
-        ),
+        backgroundColor: Colors.black,
+        body: PlaySound(),
       ),
     ),
   );
+}
+
+class PlaySound extends StatelessWidget {
+  void noteSound(int noteNum) {
+    final player = AudioPlayer();
+    player.play(AssetSource('note$noteNum.wav'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: TextButton(
+              style: TextButton.styleFrom(
+                  backgroundColor: Colors.red, foregroundColor: Colors.white),
+              onPressed: () {
+                noteSound(1);
+              },
+              //style: TextButton.styleFrom(foregroundColor: Colors.amber),
+              child: Text('Press Me'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              style: TextButton.styleFrom(
+                  backgroundColor: Colors.amber, foregroundColor: Colors.black),
+              onPressed: () {
+                noteSound(2);
+              },
+              //style: TextButton.styleFrom(foregroundColor: Colors.amber),
+              child: Text('Press Me'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              style: TextButton.styleFrom(
+                  backgroundColor: Colors.green, foregroundColor: Colors.white),
+              onPressed: () {
+                noteSound(3);
+              },
+              //style: TextButton.styleFrom(foregroundColor: Colors.amber),
+              child: Text('Press Me'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              style: TextButton.styleFrom(
+                  backgroundColor: Colors.grey, foregroundColor: Colors.black),
+              onPressed: () {
+                noteSound(4);
+              },
+              //style: TextButton.styleFrom(foregroundColor: Colors.amber),
+              child: Text('Press Me'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              style: TextButton.styleFrom(
+                  backgroundColor: Colors.blue, foregroundColor: Colors.white),
+              onPressed: () {
+                noteSound(5);
+              },
+              //style: TextButton.styleFrom(foregroundColor: Colors.amber),
+              child: Text('Press Me'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              style: TextButton.styleFrom(
+                  backgroundColor: Colors.teal, foregroundColor: Colors.white),
+              onPressed: () {
+                noteSound(6);
+              },
+              //style: TextButton.styleFrom(foregroundColor: Colors.amber),
+              child: Text('Press Me'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              style: TextButton.styleFrom(
+                  backgroundColor: Colors.indigo,
+                  foregroundColor: Colors.white),
+              onPressed: () {
+                noteSound(7);
+              },
+              //style: TextButton.styleFrom(foregroundColor: Colors.amber),
+              child: Text('Press Me'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
